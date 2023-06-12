d9edd089389b33c18537e53a38d52a625e1f165c
Tommaso <tommaso.brini@studio.unibo.it>
2023-06-12 15:54:47 +0200

    Add DaoVehicle

--- SharedMobility/src/Dao/DaoVehicle.java
+++ SharedMobility/src/Dao/DaoVehicle.java
@@ -1,4 +1,15 @@
 package Dao;
 
+import Model.Vehicle;
+
+import java.util.UUID;
+
 public interface DaoVehicle {
+    boolean insert(Vehicle vehicle);
+
+    boolean update(Vehicle vehicle);
+
+    boolean delete(Vehicle vehicle);
+
+    Vehicle get(UUID id);
 }
