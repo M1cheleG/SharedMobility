e839c3ed2858d71f1febe9d05259f88b2c7f3bc3
Lollorenzo8 <lorenzo.francia1@gmail.com>
2023-06-12 15:23:40 +0200

    Update DB

--- SharedMobility/src/DB.java
+++ SharedMobility/src/DB.java
@@ -1,2 +1,26 @@
+import Model.User;
+import Model.Vehicle;
+import java.util.ArrayList;
+import java.util.List;
+
+//DB con Singleton
 public class DB {
+    private static DB Internalinstance;
+    private List<User> user;
+    private List<Vehicle> vehicle;
+    private DB(){
+        user = new ArrayList<>();
+        vehicle = new ArrayList<>();
+    }
+
+    public static DB getInstance(){
+        if(Internalinstance == null){
+            synchronized (DB.class){
+                if(Internalinstance == null){
+                    Internalinstance = new DB();
+                }
+            }
+        }
+        return Internalinstance;
+    }
 }
