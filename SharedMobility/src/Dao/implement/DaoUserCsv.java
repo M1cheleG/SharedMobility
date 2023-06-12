254af10f3a94f99778a74c9aad3d1c002be2ce6a
MicheleG <myke.greco@gmail.com>
2023-06-12 15:32:49 +0200

    Dao Set up

--- SharedMobility/src/Dao/implement/DaoUserCsv.java
+++ SharedMobility/src/Dao/implement/DaoUserCsv.java
@@ -1,4 +1,45 @@
 package Dao.implement;
 
-public class DaoUserCsv {
+import Dao.DaoUser;
+import Model.User;
+
+import java.nio.file.Path;
+import java.util.List;
+import java.util.TreeMap;
+import java.util.UUID;
+
+public class DaoUserCsv implements DaoUser {
+
+    private Path usersCsv;
+    private TreeMap<UUID,User> idsToUsers;
+
+    public DaoUserCsv(Path usersCsv) {
+        this.usersCsv = usersCsv;
+        this.idsToUsers= new TreeMap<>();
+    }
+
+    @Override
+    public boolean insert(User user) {
+        return false;
+    }
+
+    @Override
+    public boolean update(User user) {
+        return false;
+    }
+
+    @Override
+    public boolean delete(UUID id) {
+        return false;
+    }
+
+    @Override
+    public User get(UUID id) {
+        return null;
+    }
+
+    @Override
+    public List<User> getAll() {
+        return null;
+    }
 }
--- SharedMobility/src/TestSharedMobility.java
+++ SharedMobility/src/TestSharedMobility.java
@@ -1,5 +1,5 @@
 public class TestSharedMobility {
     public static void main(String[] args) {
-
+        System.out.println("ciao");
     }
 }
