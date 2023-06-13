7af88ca1ac7c317af3d7db013746bda7efe43430
MicheleG <myke.greco@gmail.com>
2023-06-13 11:46:19 +0200

    DaoVehicleCsv

--- SharedMobility/src/Dao/implement/DaoVehicleCsv.java
+++ SharedMobility/src/Dao/implement/DaoVehicleCsv.java
@@ -1,4 +1,108 @@
 package Dao.implement;
 
-public class DaoVehicleCsv {
+import Dao.DaoVehicle;
+import Model.User;
+import Model.Vehicle;
+import Model.Vehicles.*;
+import com.sun.source.tree.Tree;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.List;
+import java.util.TreeMap;
+import java.util.UUID;
+
+public class DaoVehicleCsv implements DaoVehicle {
+
+    private Path vehicleCsv;
+    private TreeMap<UUID, Vehicle> idsToVehicle;
+
+    public DaoVehicleCsv(Path vehicleCsv) {
+        this.vehicleCsv = vehicleCsv;
+        this.idsToVehicle= new TreeMap<>();
+
+        try {
+            if(!Files.exists(vehicleCsv)) {
+                Files.createFile(vehicleCsv);
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        try (BufferedReader br =Files.newBufferedReader(vehicleCsv)){
+            String line;
+
+            while ((line= br.readLine())!=null) {
+
+
+
+            }
+
+            }catch (IOException e){
+            e.printStackTrace();
+        }
+        }
+
+    @Override
+    public boolean insert(Vehicle vehicle) {
+
+        return false;
+    }
+
+    @Override
+    public boolean update(Vehicle vehicle) {
+        return false;
+    }
+
+    @Override
+    public boolean delete(Vehicle vehicle) {
+        return false;
+    }
+
+    @Override
+    public Vehicle get(UUID id) {
+        return null;
+    }
+
+    public boolean save(){
+        try (BufferedWriter bw = Files.newBufferedWriter(this.vehicleCsv)) {
+            for (Vehicle vehicle : idsToVehicle.values()) {
+                List<String> temp = Arrays.asList();
+                if(vehicle instanceof Bicycle){
+                }else if(vehicle instanceof Car){
+                }else if(vehicle instanceof MotoScooter) {
+                } else if (vehicle instanceof Scooter) {
+                } else if (vehicle instanceof Van) {
+
+                }
+                List<String> values = Arrays.asList();
+
+
+
+
+                bw.write(String.join(":", values));
+                bw.newLine();
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+
+
+
+        return false;
+    }
 }
+/* if(vehicle instanceof Bicycle){
+                }else if(vehicle instanceof Car){
+                }else if(vehicle instanceof MotoScooter) {
+                } else if (vehicle instanceof Scooter) {
+                } else if (vehicle instanceof Van) {
+
+                } */
